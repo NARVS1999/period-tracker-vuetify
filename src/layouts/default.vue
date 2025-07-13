cb6c0e6b375bc73fc691a56c3db3855add6fa437
John Paul <johnpaulnarvasa6@gmail.com>
2025-07-13 21:12:56 +0800

    added layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,34 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-app>
+    <!-- AppBar -->
+    <v-app-bar color="primary" density="comfortable">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
-  <AppFooter />
+      <!-- Dedicated icon for period tracker -->
+
+      <v-app-bar-title class="text-uppercase"> <v-icon
+          class="mr-2">mdi-calendar-heart</v-icon>MyCycleMate</v-app-bar-title>
+
+
+    </v-app-bar>
+
+    <!-- Optional NavigationDrawer (hidden on desktop) -->
+    <v-navigation-drawer color="primary" v-model="drawer" temporary location="left">
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-home" title="Home" to="/" />
+        <v-list-item prepend-icon="mdi-chart-bar" title="Reports" to="/reports" />
+        <v-list-item prepend-icon="mdi-cog" title="Settings" to="/settings" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Main content -->
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue'
+const drawer = ref(false)
 </script>
